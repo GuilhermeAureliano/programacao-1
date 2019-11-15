6bf69a0d0739184cc035cd675e51c15e6a55dfb1
Guilherme Aureliano <49173189+GuilhermeAureliano@users.noreply.github.com>
2019-11-15 13:35:33 -0300

    Create scroll.py
    questão de prova

---
+++ unidade9/scroll.py
@@ -0,0 +1,21 @@
+# Universidade Federal de Campina Grande - UFCG
+# Programação 1 - 2019.2
+# Guilherme Aureliano
+
+def scroll(m):
+	for i in range(len(m) - 1):
+		m[i], m[i + 1] = m[i + 1], m[i]
+	for i in range(len(m)):
+		for j in range(len(m[0])):
+			if i == len(m) - 1:
+				m[i][j] *= 0
+	return m
+  
+m = [[1,2,3],
+     [4,5,6],
+     [7,8,9],
+	   [10, 11, 12]]
+assert scroll(m) == [[4, 5, 6],
+                    [7, 8, 9],
+                    [10, 11, 12],
+                    [0, 0, 0]]
